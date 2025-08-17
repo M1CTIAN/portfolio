2cd7c61ded7d38fc31fb161ee88f5a2f20e28569
M1CTIAN <pkanojiya3@gmail.com>
2025-08-17 15:46:54 +0530

    Add Vercel Analytics - 2

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -50,6 +50,7 @@ export default function RootLayout({ children }) {
           <div data-scroll-container ref={scrollRef}>
             {children}
           </div>
+          <Analytics />
         </body>
       </CursorProvider>
     </html>
